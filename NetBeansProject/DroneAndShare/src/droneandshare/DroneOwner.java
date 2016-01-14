5bad24ee8f4f8a31a03af25a2fed31492c654088
lgortan <gortan.laura@gmail.com>
2016-01-14 16:47:00 +0100

    Add comments to DroneOwner

--- NetBeansProject/DroneAndShare/src/droneandshare/DroneOwner.java
+++ NetBeansProject/DroneAndShare/src/droneandshare/DroneOwner.java
@@ -6,49 +6,68 @@
 package droneandshare;
 
 /**
+ * Class DroneOwner represents the drone owner. Get nickname of DroneOwner and
+ * the Camera used; ask to AppControl to shot a picture and to Drone to install
+ * a new camera.
  *
  * @author AWT
  */
 public class DroneOwner {
-    
+
     private String nickName;
     private Camera camera;
     private AppControl app;
     private Drone drone;
-    
-    
-    public DroneOwner(String nickName, Camera camera, AppControl app, Drone drone){
-        this.nickName= nickName;
-        this.camera= camera;
-        this.app= app;
-        this.drone= drone;
-        System.out.println("Welcome "+ nickName+ "!!");
-        System.out.println(nickName+ "has installed camera: "+ camera);
+
+    /**
+     * Create a constructor of DroneOwner
+     *
+     * @param nickName is the nick name of drone owner
+     * @param camera is the model of the camera
+     * @param app is the name of AppControl
+     * @param drone is the model of the drone
+     */
+    public DroneOwner(String nickName, Camera camera, AppControl app, Drone drone) {
+        this.nickName = nickName;
+        this.camera = camera;
+        this.app = app;
+        this.drone = drone;
+        System.out.println("Welcome " + nickName + "!!");
+        System.out.println(nickName + "has installed camera: " + camera);
     }
-    
-    public String getNickName(){
+
+    public String getNickName() {
         return nickName;
     }
-    
-    public Camera getCamera(){
+
+    public Camera getCamera() {
         return camera;
     }
-    
-    public void setCamera(Camera modello){
-        camera= modello;
+
+    public void setCamera(Camera modello) {
+        camera = modello;
     }
-    
-    public void installMyCamera(){
+
+    /**
+     * Ask drone to install a new camera
+     */
+    public void installMyCamera() {
         drone.installNewCamera(camera);
-        System.out.println(nickName+ "your camera has been installed.");
+        System.out.println(nickName + "your camera has been installed.");
     }
-    
-    public void askForPicture(){
+
+    /**
+     * Ask app to shot a picture
+     */
+    public void askForPicture() {
         app.shotAPicture();
     }
 
-    void askForShowPicture() {
+    /**
+     * Ask app to show a picture
+     */
+    public void askForShowPicture() {
         app.showPicture();
     }
-    
+
 }
