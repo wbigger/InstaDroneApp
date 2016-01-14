3d62c42cdd45712a36a826787fc7b50159639bfa
CORSO <CORSO@ENETMI05>
2016-01-14 16:48:13 +0100

    added documentation

--- NetBeansProject/DroneAndShare/src/droneandshare/Drone.java
+++ NetBeansProject/DroneAndShare/src/droneandshare/Drone.java
@@ -6,35 +6,58 @@
 package droneandshare;
 
 /**
- *
- * @author CORSO
+ * Creation of a class named Drone
+ * @author Vincenzo e Luca
  */
 
 public class Drone {
-    
+    /**Drone's attribute
+     * 
+     */
     private Camera mCamera;
-    
+    /**Costructor of Drone class
+     * 
+     * @param camera 
+     */
     public Drone (Camera camera) {
         mCamera=camera;
+        /**Print a video text that shows the string "Loading new camera in progress!"
+         * 
+         */
         System.out.println("Loading new camera in progress! ");
               
     }
+    /**Overloading of the costructor of the class Drone created for the case in which there is no camera inserted
+     * 
+     */
         public Drone () {
             this.mCamera=new Camera("insert in progress");
+            /**Print a video text that shows an error
+             * 
+             */
              System.out.println(" no use camera! ");
         }
         
-    
+    /**Creation of a method to install a camera on drone
+     * 
+     * @param camera 
+     */
     public void installNewCamera (Camera camera){
        mCamera=camera;
-        System.out.println("La camera e' stata installata!! ");
+     System.out.println("La camera e' stata installata!! ");
         
     }
-
+/**get method used to return mCamera because it was private
+ * 
+ * @return mCamera
+ */
     public Camera getmCamera() {
         return mCamera;
     }
-
+/** return a picture from the camera
+ * 
+ * @return mCamera.shotAPicture()
+ */
     public Picture shotAPicture(){
        return mCamera.shotAPicture();
     }
